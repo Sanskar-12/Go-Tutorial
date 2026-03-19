b97c32afd6a5d140461390b16fadba64db3080cb
Sanskar-12 <sanskarv2004@gmail.com>
2026-03-19 13:17:01 +0530

    Feature: Different types to declare variables in GO

---
+++ 2_VariableDeclaration/variablesingo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var productName string // Declare a variable but no value is assigned
+	var productPrice int=1000 // Declare a variable and assign value
+	var companyName = "Apple Inc." // Declare a variable and assign a value but type is not declared
+
+	// *** mostly used ***
+	category:="Electronics" // Declare a variable and assign a value but type is not declared and var is not used
+
+	fmt.Println("Product Name is ",productName," and price is ",productPrice," and company name is ",companyName, " and category is ",category)
+}
