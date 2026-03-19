5159952354089f8e2c2182d8f33c497465e32cb9
Sanskar-12 <sanskarv2004@gmail.com>
2026-03-19 13:17:31 +0530

    Feature: Different types to declare variables in GO

---
+++ 1_HelloWorld/helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// print a String
+	fmt.Println("Hello world")
+	
+	// declare variables
+	var productName string = "Iphone 16"
+	fmt.Println("Product Name is ",productName)
+}
